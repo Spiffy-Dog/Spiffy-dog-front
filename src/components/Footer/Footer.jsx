7cbe73080fa051dd6e8e081f50db5ab39298304c
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-21 18:42:08 +0300

    before the storm

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import style from './Footer.module.scss';
 import SocialLinks from '../SocialLinks';
+
 const Footer = () => {
   return (
     <footer>
-      <SocialLinks />
+      <SocialLinks fill="#556B2F" backgroundColor="fff" />
     </footer>
   );
 };
--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -1,9 +1,11 @@
 import { ReactComponent as Insta } from '../../assets/images/insta.svg';
 
 export default function SocialLinks({ backgroundColor, fill }) {
+  const styles = { insta: { fill: `${fill}` } };
+
   return (
     <div>
-      <Insta />
+      <Insta style={styles.insta} />
     </div>
   );
 }
