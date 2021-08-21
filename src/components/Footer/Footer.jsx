7d003ee9c3cb92e40d542cb5ab4d71fc8b183db5
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-21 18:21:02 +0300

    before storm

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import style from './Footer.module.scss';
+import SocialLinks from '../SocialLinks';
 const Footer = () => {
-  return <footer></footer>;
+  return (
+    <footer>
+      <SocialLinks />
+    </footer>
+  );
 };
 
 export default Footer;
---
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as Insta } from '../../assets/images/insta.svg';
+
+export default function SocialLinks({ backgroundColor, fill }) {
+  return (
+    <div>
+      <Insta />
+    </div>
+  );
+}
