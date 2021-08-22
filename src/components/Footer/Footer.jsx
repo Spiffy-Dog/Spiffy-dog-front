017f93b7f28c6efbb35d9a76310697551ee9aad1
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-22 22:19:57 +0300

    styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,16 @@
 import style from './Footer.module.scss';
+import { Link } from 'react-router-dom';
 import SocialLinks from '../SocialLinks';
+import Logo from '../Logo';
+import styled from 'styled-components';
 
 const Footer = () => {
   return (
     <footer>
-      <SocialLinks fill="#556B2F" backgroundColor="white" />
+      <div className={style.wrapper}>
+        <Logo height="124px" fill="white" />
+        <SocialLinks fill="#556B2F" backgroundColor="white" />
+      </div>
     </footer>
   );
 };
